582de19b6726541b3e5b2f5045211aa432452127
Gus Glover <gus.glov3r@gmail.com>
2015-08-07 13:58:34 -0700

    dummy commit to test auto-deploy

--- home/index.php
+++ home/index.php
@@ -8,6 +8,8 @@ include_once('partials/header.php');
 
 ?>
 
+<!-- How did he make those stripes, what the hell? -->
+
 <p>
 I'm a fresh graduate of the University of Washington currently bumbling through the early trials of entrepreneurship. Welcome to the hub for all my 
 creative output.
