cfa29a6dde650085cd939abd7e7428d15e8f2dae
Gus Glover <gus.glov3r@gmail.com>
2016-11-11 12:42:11 -0800

    fixing ssl for subdomains

--- home/blog/goodwill-alterations-two-tone-polo/index.php
+++ home/blog/goodwill-alterations-two-tone-polo/index.php
@@ -9,7 +9,9 @@ include_once('../../partials/header.php');
 ?>
 
 <h1>Two-tone Polo</h1>
-<h4>Goodwill Alterations #4 | <a href="/blog/goodwill-alterations-asymmetric-cargo-shorts">< prev</a></h4>
+<h4>Goodwill Alterations #4 | <a href="/blog/goodwill-alterations-asymmetric-cargo-shorts">< prev</a> - <a href="/blog/goodwill-alterations-asymmetric-corduroy-thigh-pockets">next ></a></h4>
+
+<p>The simple concept here is to take a cheap polo and extend the arms down to just below the elbow. This is my first time working properly with an annoying knit fabric.</p>
 
 <?php
 
@@ -24,6 +26,8 @@ $caption = "<strong>$4.99</strong> H&M Polo";
 include('../../partials/lightbox_img.php');
 
 ?>
+
+<p>These two polos were found adjacent to each other on the rack, both too small. I had to expand out the midsection with a vertical stripe along the side seam to avoid lookng like a pressed sausage.</p>
 
 <div class="expense-total">Total Expenses: <strong>$10.98</strong></div>
 
